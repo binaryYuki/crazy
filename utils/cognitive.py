f175abe18e10c4771fcdea3aed3562575908b73a
binaryYuki <60097976+binaryYuki@users.noreply.github.com>
2024-10-13 14:16:44 +0100

    new fallback api

--- utils/cognitive.py
+++ utils/cognitive.py
@@ -29,15 +29,17 @@ async def cogRanker(text: str) -> tuple:
         response = await client.post(url, headers=headers, json=body)
 
     data = response.json()
-    hateRate = data["categoriesAnalysis"][0]["severity"]
-    selfHarmRate = data["categoriesAnalysis"][1]["severity"]
-    sexualRate = data["categoriesAnalysis"][2]["severity"]
-    violenceRate = data["categoriesAnalysis"][3]["severity"]
-
-    print(f"Hate Rate: {hateRate}")
-    print(f"Self Harm Rate: {selfHarmRate}")
-    print(f"Sexual Rate: {sexualRate}")
-    print(f"Violence Rate: {violenceRate}")
+    try:
+        hateRate = data["categoriesAnalysis"][0]["severity"]
+        selfHarmRate = data["categoriesAnalysis"][1]["severity"]
+        sexualRate = data["categoriesAnalysis"][2]["severity"]
+        violenceRate = data["categoriesAnalysis"][3]["severity"]
+        print(f"Hate Rate: {hateRate}")
+        print(f"Self Harm Rate: {selfHarmRate}")
+        print(f"Sexual Rate: {sexualRate}")
+        print(f"Violence Rate: {violenceRate}")
+    except Exception as e:
+        print(f"Error: {e}")
 
     # 综合评分 计算是否允许进行下一步操作
     if sexualRate >= 2:
