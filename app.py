f34b7b09903ceaed01b8953104019c841dcaef67
binaryYuki <60097976+binaryYuki@users.noreply.github.com>
2024-10-13 15:00:10 +0100

    0 runtime

--- app.py
+++ app.py
@@ -71,12 +71,12 @@ async def response(request: Request):
         print(f"Data: {data}")
     except Exception as e:
         return JSONResponse(
-            content={"success": False, "response": shitWords(), "time": "0", "run": False})
+            content={"success": False, "response": await shitWords(), "time": "0", "run": False})
 
     result, bonus = await cogRanker(data)
     if not result:
         return JSONResponse(
-            content={"success": True, "run": False, "time": str(0), "response": shitWords()})
+            content={"success": True, "run": False, "time": str(0), "response": await shitWords()})
     else:
         rank = await analyze_text_async(data)
         print(f"Rank: {rank}", f"Bonus: {bonus}")
